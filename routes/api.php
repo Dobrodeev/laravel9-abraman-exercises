c1e0eb409ff56821f9b6bf487c80847500f1b23a
Добродеев Валера <33656818+Dobrodeev@users.noreply.github.com>
2022-08-04 15:10:02 +0300

    добавили заготоку api-роута

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::get('/myapi', function () {
+    echo 'api/myapi<br>';
+    // return view('welcome');
+    echo 'Route from api.php';
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
