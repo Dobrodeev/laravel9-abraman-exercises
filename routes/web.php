b2da64f9a0d294ca372ea5471100d58ee8a51ae3
Добродеев Валера <33656818+Dobrodeev@users.noreply.github.com>
2022-08-04 15:11:44 +0300

    добавили роут с параметрами.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BooksController;
+use App\Http\Controllers\UsersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +18,22 @@ use App\Http\Controllers\BooksController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// с помощью Laravel Excel выгружаем данные в файл users.xlsx
+Route::get('export-users/', [BooksController::class, 'exportUsers']);
+Route::get('export-menus/', [BooksController::class, 'exportMenus']);
+// Import
+Route::get('import-users', [UsersController::class, 'import']);
+
+Route::get('/user/{id}/{user2id}', function ($id, $user2) {
+    echo '/user/{id}/{user2id}<br>';
+    echo $id.'<br>';
+    echo $user2;
+});
 Route::get('/books', [BooksController::class, 'show']);
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
+
+
