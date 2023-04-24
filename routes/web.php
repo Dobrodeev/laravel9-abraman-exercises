6b4e05419d88e4d392f0061923d43a8bc76c9748
Коломаренко Дмитрій <kolobok575lg@gmail.com>
2023-04-24 15:49:57 +0300

    Laravel було перевстановлено для позбуття помилок.

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BooksController;
-use App\Http\Controllers\UsersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -10,34 +8,11 @@ use App\Http\Controllers\UsersController;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::get('/greeting', function () {
-    return 'Hello World';
-});
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-// с помощью Laravel Excel выгружаем данные в файл users.xlsx
-Route::get('export-users/', [BooksController::class, 'exportUsers']);
-Route::get('export-menus/', [BooksController::class, 'exportMenus']);
-// Import
-Route::get('import-users', [UsersController::class, 'import']);
-
-Route::get('/user/{id}/{user2id}', function ($id, $user2) {
-    echo '/user/{id}/{user2id}<br>';
-    echo $id.'<br>';
-    echo $user2;
-});
-Route::get('/books', [BooksController::class, 'show']);
-
-Route::group(['prefix' => 'admin'], function () {
-    Voyager::routes();
-});
-
-
